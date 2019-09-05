6cef389a0ffab859fb473183678741598cd209a0
IDCs <idc.dts@gmail.com>
2019-09-05 10:00:11 +0100

    Removing gameId based whitelist functionality - Previously only "supported" games were able to use the qbms extension;   this defeats the purpose of the extension and would block 3rd party extensions   from using qbms without being forced to submit a pull request   for their gameId to be added to the whitelist (this wasn't secure anyway)

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,20 @@ import { IListEntry, IQBMSOptions, QBMSFunc, QBMSOperationType } from './types';
 
 import { log, selectors, types } from 'vortex-api';
 
-const supportedGames = ['residentevil22019', 'devilmaycry5'];
+const attribGames = ['residentevil22019', 'devilmaycry5'];
 
-function isSupported(state: types.IState) {
+function showAttrib(state: types.IState) {
   const gameMode = selectors.activeGameId(state);
 
-  return supportedGames.indexOf(gameMode) !== -1;
+  return attribGames.indexOf(gameMode) !== -1;
 }
 
 function init(context: types.IExtensionContext) {
   context.registerDashlet('QBMS Support', 1, 2, 250, AttribDashlet,
-    isSupported, () => ({}), undefined);
+    showAttrib, () => ({}), undefined);
 
   context.once(() => {
-    context.api.events.on('quickbms-operation', (gameId: string,
-                                                 bmsScriptPath: string,
+    context.api.events.on('quickbms-operation', (bmsScriptPath: string,
                                                  archivePath: string,
                                                  inPath: string,
                                                  opType: QBMSOperationType,
@@ -29,29 +28,27 @@ function init(context: types.IExtensionContext) {
         callback(err, undefined);
       };
 
-      if (supportedGames.indexOf(gameId) !== -1) {
-        const qbms = require('./quickbms');
-        let qbmsFunc: QBMSFunc;
-        switch (opType) {
-          case 'extract':
-            qbmsFunc = qbms.extract;
-            break;
-          case 'reimport':
-            qbmsFunc = qbms.reImport;
-            break;
-          case 'write':
-            qbmsFunc = qbms.write;
-            break;
-          case 'list':
-          default:
-            return qbms.list(archivePath, bmsScriptPath, inPath, options)
-              .then(listEntries => callback(undefined, listEntries))
-              .catch(err => reportError(err));
-        }
-        return qbmsFunc(archivePath, bmsScriptPath, inPath, options)
-          .then(() => callback(undefined, undefined)) // All went well.
-          .catch(err => reportError(err));
+      const qbms = require('./quickbms');
+      let qbmsFunc: QBMSFunc;
+      switch (opType) {
+        case 'extract':
+          qbmsFunc = qbms.extract;
+          break;
+        case 'reimport':
+          qbmsFunc = qbms.reImport;
+          break;
+        case 'write':
+          qbmsFunc = qbms.write;
+          break;
+        case 'list':
+        default:
+          return qbms.list(archivePath, bmsScriptPath, inPath, options)
+            .then(listEntries => callback(undefined, listEntries))
+            .catch(err => reportError(err));
       }
+      return qbmsFunc(archivePath, bmsScriptPath, inPath, options)
+        .then(() => callback(undefined, undefined)) // All went well.
+        .catch(err => reportError(err));
     });
   });
 
