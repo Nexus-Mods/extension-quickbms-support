b73c07b8f527dd804aba748c7f8c44deddc8bbb2
idcz <idc.dts@gmail.com>
2020-06-15 11:52:11 +0100

    Avoid showing notifications when an unsupported game is active
    It seems to be common for users to run qbms operations and not wait
    for them to complete. In the meantime they may change to a different
    game mode, at which point they may receive qbms notifications and may
    erroneously assume that it's the currently active game reporting these.
    
    This extension REALLY needs re-writing to function as a module and not
    an event based system.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import AttribDashlet from './AttribDashlet';
 import { IListEntry, IQBMSOptions, QBMSFunc, QBMSOperationType, QuickBMSError } from './types';
 
-import { log, selectors, types } from 'vortex-api';
+import { log, selectors, types, util } from 'vortex-api';
 
 const attribGames = ['residentevil22019', 'devilmaycry5'];
+
+// QBMS may take a while to complete its operations.
+//  during this time users may switch between gameModes
+//  resulting in them receiving notifications from QBMS
+//  failures. We're going to use this boolean to suppress
+//  error notification under these circumstances.
+// TODO: this is a hack, this extension needs to be re-written
+//  to function as a module rather than be event based.
+let _GAMEMODE_SUPPORTED: boolean = false;
 
 function showAttrib(state: types.IState) {
   const gameMode = selectors.activeGameId(state);
@@ -16,6 +25,9 @@ function init(context: types.IExtensionContext) {
     showAttrib, () => ({}), undefined);
 
   context.once(() => {
+    context.api.events.on('gamemode-activated', (gameMode: string) => {
+      _GAMEMODE_SUPPORTED = attribGames.indexOf(gameMode) !== -1;
+    });
     context.api.events.on('quickbms-operation', (bmsScriptPath: string,
                                                  archivePath: string,
                                                  inPath: string,
@@ -23,12 +35,24 @@ function init(context: types.IExtensionContext) {
                                                  options: IQBMSOptions,
                                                  // tslint:disable-next-line: max-line-length
                                                  callback: (err: Error, data: IListEntry[]) => void) => {
+      const state = context.api.store.getState();
+      const activeGameId = selectors.activeGameId(state);
+      if (attribGames.indexOf(activeGameId) === -1) {
+        // Not a QBMS game.
+        return callback(new util.ProcessCanceled('[QBMS] active game changed'), undefined);
+      }
+
       const reportError = (err: Error) => {
         const errMessage = (err instanceof QuickBMSError)
           ? err.message + '\n\n' + (err as QuickBMSError).errorLines
           : err.message;
-        log('error', 'qbms encountered an error', errMessage);
-        callback(err, undefined);
+        if (_GAMEMODE_SUPPORTED) {
+          log('error', 'qbms encountered an error', errMessage);
+          callback(err, undefined);
+        } else {
+          log('info', 'qbms encountered an error', errMessage);
+          callback(new util.ProcessCanceled('[QBMS] active game changed'), undefined);
+        }
       };
 
       const qbms = require('./quickbms');
